46e2986565102a80d72aa4fb3afb207d6e746bc4
alakim <alakm@rambler.ru>
2017-03-08 00:41:46 +0300

    Simple data editor

--- js/horn.js
+++ js/horn.js
@@ -7,11 +7,73 @@ var Horn = (function($D){
 		}
 	})();
 
+	function getItem(data, path){
+		// console.log(1, path);
+		if(typeof(path)=='string') path = path.split('/');
+		// console.log(2, path);
+		if(!path.length) return data;
+		if(!path[0].length) return getItem(data, path.splice(1));
+		var step = path[0];
+		if(data[step]) return getItem(data[step], path.splice(1));
+	}
+
+	function append(data, change){
+		var itm = getItem(data, change.path);
+		if(itm instanceof Array)
+			itm.push(change.item);
+		else 
+			console.error(itm, ' is not array');
+	}
+
+	function applyChange(data, change){
+		switch(change.type){
+			case 'append':
+				append(data, change);
+				break;
+			default:
+				break;
+		}
+	}
+
+	function clone(itm){
+		if(itm instanceof Array){
+			var newItm = [];
+			for(var el,i=0; el=itm[i],i<itm.length; i++){
+				newItm.push(clone(el));
+			}
+			return newItm;
+		}
+		if(typeof(itm)=='object'){
+			var newObj = {};
+			for(var k in itm){
+				newObj[k] = clone(itm[k]);
+			}
+			return newObj;
+		}
+		if(typeof(itm)=='function') return null;
+		return itm;
+	}
 
 	function Horn(data){
+		var changes = [];
 		return {
 			getData: function(){
 				return data;
+			},
+			append: function(path, item){
+				changes.push({
+					type:'append',
+					path: path,
+					item: item
+				});
+			},
+			applyChanges: function(){
+				var dd = clone(data);
+				$D.each(changes, function(chg){
+					applyChange(dd, chg);
+				});
+				console.log('Было: %o, стало: %o', data, dd);
+				return Horn(dd);
 			}
 		};
 	}
--- samples/catalog/catalogEditor.js
+++ samples/catalog/catalogEditor.js
@@ -1,24 +1,62 @@
 var CatalogEditor = (function($, $H){
+	var px = $H.unit('px');
 	$H.writeStylesheet({
 		'body':{
-			fontFamily: 'Verdana, Arial, Sans-serif'
+			fontFamily: 'Verdana, Arial, Sans-serif',
+			' .dialogPanel':{
+				border: px(1)+' solid #ccc'
+			}
 		}
 	});
 
 	var db = {getData: function(){return {books:[]};}};
 
-	function init(){
-		db = Horn(catalogData);
-
+	function displayData(db){
 		$('#pnlEditor').html((function(){with($H){
 			return div(
 				apply(db.getData().books, function(bk){
 					return div(
 						bk.author, ': ', bk.title
 					);
-				})
+				}),
+				div(
+					button({'class':'btAdd'}, 'Add'),
+					div({'class':'dialogPanel dlgAdd', style:'display:none'},
+						table(
+							tr(
+								td('Title'),
+								td(input({type:'text', 'class':'tbTitle'}))
+							),
+							tr(
+								td('Author'),
+								td(input({type:'text', 'class':'tbAuthor'}))
+							)
+						),
+						div(
+							button({'class':'btOK'}, 'OK'),
+							button({'class':'btCancel'}, 'Cancel')
+						)
+					)
+				)
 			);
-		}})());
+		}})())
+		.find('.btAdd').click(function(){
+			$('.dlgAdd').show();
+		}).end()
+		.find('.dlgAdd .btCancel').click(function(){
+			$('.dlgAdd').hide();
+		}).end()
+		.find('.dlgAdd .btOK').click(function(){
+			var title = $('.dlgAdd .tbTitle').val(),
+				author = $('.dlgAdd .tbAuthor').val();
+			db.append('/books/', {title:title, author:author});
+			displayData(db.applyChanges());
+			$('.dlgAdd').hide();
+		}).end();
+	}
+
+	function init(){
+		displayData(Horn(catalogData));
 	}
 
 	$(init);
